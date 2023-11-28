7f9be90480b2f5f53f23ecd061b087940fd23a23
budres <ondrej@ondrejprokes.com>
2023-11-28 13:30:50 +0100

    sync, added list of list handlers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 const listRouter = require("./controller/list-controller")
 const userRouter = require("./controller/user-controller")
@@ -9,6 +10,7 @@ const prefix = "/api"
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 
 app.get(prefix + "/", (req, res) => {
